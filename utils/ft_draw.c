557bac3bdb33aa177fe20c5db07c908edcfa9e08
TheTerror <johnmrfaye12@gmail.com>
2023-02-16 16:14:31 +0100

    Organisation du travail et ajout de la mlx

---
+++ utils/ft_draw.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_draw.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 00:39:33 by TheTerror         #+#    #+#             */
+/*   Updated: 2023/02/16 01:20:15 by TheTerror        ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_utils.h"
+
+void	ft_join_pt(t_img *img, double x0, double m, t_coord *pt, int color)
+{
+	double	y;
+
+	y = 0;
+	while (x0 <= pt->x)
+	{
+		y = m * x0 + (pt->y - m * pt->x);
+		if (y >= 0)
+			my_mlx_pixel_put(img, x0, y, color);
+		x0++;
+	}
+}
+
+void	ft_vert_line(t_img *img, double y0, t_coord *pt, int color)
+{
+	double	x;
+
+	x = pt->x;
+	while (y0 <= pt->y)
+	{
+		my_mlx_pixel_put(img, x, y0, color);
+		y0++;
+	}
+}
+
+void	ft_hor_line(t_img *img, double x0, t_coord *pt, int color)
+{
+	double	y;
+
+	y = pt->y;
+	while (x0 <= pt->x)
+	{
+		my_mlx_pixel_put(img, x0, y, color);
+		x0++;
+	}
+}
+
+void	ft_drw_line(t_img *img, t_coord *pt1, t_coord *pt2, int color)
+{
+	double	dx;
+	double	dy;
+
+	dx = pt2->x - pt1->x;
+	dy = (pt2->y - pt1->y);
+	if (dx == 0)
+	{
+		if (dy < 0)
+			ft_vert_line(img, pt2->y, pt1, color);
+		if (dy > 0)
+			ft_vert_line(img, pt1->y, pt2, color);
+	}
+	else if (dy == 0)
+	{
+		if (dx < 0)
+			ft_hor_line(img, pt2->x, pt1, color);
+		if (dx > 0)
+			ft_hor_line(img, pt1->x, pt2, color);
+	}
+	else if (dx < 0)
+		ft_join_pt(img, pt2->x, (dy / dx), pt1, color);
+	else if (dx > 0)
+		ft_join_pt(img, pt1->x, (dy / dx), pt2, color);
+}
---
+++ utils/ft_utils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 22:44:28 by TheTerror         #+#    #+#             */
+/*   Updated: 2023/02/16 01:24:59 by TheTerror        ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_UTILS_H
+# define FT_UTILS_H
+
+#include "../ft_fdf.h"
+
+void	ft_drw_line(t_img *img, t_coord *pt1, t_coord *pt2, int color);
+
+#endif
